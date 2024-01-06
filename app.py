3d383e7b9e4162c6da188fd4e38f3bbac2ab3edb
ChrisCodes0206 <chrismon850@gmail.com>
2024-01-06 16:57:22 -0500

    Navbar

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
-
+# Don't forget to run the env
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return "Hello, World!"
+    return render_template('scaling.html')
+
+@app.route('/scaling')
+def scaling_page():
+    return render_template('scaling.html')
+
+@app.route('/addition')
+def addition_page():
+    return render_template('addition.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
